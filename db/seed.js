4f337150e89d55353c71b6cae2fd50400f82e269
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-12-04 15:15:51 -0600

    added createTables with first table of robots and dropTables

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,29 @@
 const client = require('./client');
 
+
+const dropTables = async() => {
+    try {
+        await client.query(`
+            DROP TABLE IF EXISTS robots;
+        `)
+    } catch (error) {
+        console.log(error);
+    }
+}
 const createTables = async() => {
     try {
-        
+        await client.query(`
+            CREATE TABLE robots (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50),
+                model VARCHAR(50),
+                image VARCHAR(200),
+                safeKids BOOLEAN,
+                company VARCHAR(70),
+                expireDate DATE,
+                releaseDate DATE
+            );
+        `)
     } catch (error) {
         console.log(error);
     }
@@ -11,6 +32,10 @@ const createTables = async() => {
 const syncAndSeed = async() => {
     await client.connect();
     console.log('CONNECTED TO DATABASE');
+    await dropTables();
+    console.log('DROPPED TABLES');
+    await createTables();
+    console.log('CREATED TABLES');
     client.end;
 }
 syncAndSeed();
