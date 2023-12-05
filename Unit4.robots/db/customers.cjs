d6eb12b847d6145e1394d9144e1ccdb7d1802680
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-12-05 15:04:20 -0600

    Additional working files

--- Unit4.robots/db/customers.cjs
+++ Unit4.robots/db/customers.cjs
@@ -1,4 +1,4 @@
-const client = require('./client');
+const client = require('./client.cjs');
 
 const createCustomer = async(name, email) => {
     try {
--- Unit4.robots/db/seed.cjs
+++ Unit4.robots/db/seed.cjs
@@ -1,6 +1,6 @@
-const client = require('./client');
-const createCustomer = require('./customers');
-const {createRobot, getAllRobots} = require('./robots');
+const client = require('./client.cjs');
+const createCustomer = require('./customers.cjs');
+const {createRobot, getAllRobots} = require('./robots.cjs');
 const createTask = require('./tasks.cjs');
 
 
@@ -64,12 +64,12 @@ const syncAndSeed = async() => {
     console.log('DROPPED TABLES');
     await createTables();
     console.log('CREATED TABLES');
-    await createRobot('Rosie', 'Technomatic 451', 'imagefile', true, 'Megatron Inc', '2102-11-11', '2023-02-12');
-    await createRobot('Hankhelper', 'Exclestor 2337', 'imagefile', false, 'Elite Bots', '2086-05-23', '2012-09-13');
-    await createRobot('Gambitbot', 'Technomatic 322', 'imagefile', true, 'Megatron Inc', '2111-09-09', '2018-09-22');
-    await createRobot('Boxotron', 'Helpmaster 888', 'imagefile', true, 'Waytogo Machines Inc', '2045-04-12', '2022-12-07');
-    await createRobot('Dusty', 'Gainfulation 9085', 'imagefile', false, 'Good Quality Bots', '2097-08-25', '2019-09-30');
-    await createRobot('Zoombator', 'Exclestor 7767', 'imagefile', true, 'Elite Bots', '2200-12-25', '2023-09-21');
+    await createRobot('Rosie', 'Technomatic 451', 'https://robohash.org/rosie.png', true, 'Megatron Inc', '2102-11-11', '2023-02-12');
+    await createRobot('Hankhelper', 'Exclestor 2337', 'https://robohash.org/hank.png', false, 'Elite Bots', '2086-05-23', '2012-09-13');
+    await createRobot('Gambitbot', 'Technomatic 322', 'https://robohash.org/megatron.png', true, 'Megatron Inc', '2111-09-09', '2018-09-22');
+    await createRobot('Boxotron', 'Helpmaster 888', 'https://robohash.org/boxter.png', true, 'Waytogo Machines Inc', '2045-04-12', '2022-12-07');
+    await createRobot('Dusty', 'Gainfulation 9085', 'https://robohash.org/dusty.png', false, 'Good Quality Bots', '2097-08-25', '2019-09-30');
+    await createRobot('Zoombator', 'Exclestor 7767', 'https://robohash.org/zoomba.png', true, 'Elite Bots', '2200-12-25', '2023-09-21');
     console.log('CREATED ROBOTS');
     await createCustomer('George Jetson', 'gjetson@galaxy.com');
     await createCustomer('Keanu Reeves', 'jwick@ustudios.com');
